a3ed4f8d98b67ad4cdce20da489adcc5044714dc
Slim Kanoun <slim.kanoun@enis.tn>
2025-10-14 17:46:14 +0100

    gitignore addition soustraction produit programme principal main

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String... arg) {
+	int a = 2;
+	int b = 3;
+	System.out.println("a + b =" + Operation.addition(a,b));
+	System.out.println("a - b =" + Operation.soustraction(a,b));
+	System.out.println("a * b =" + Operation.produit(a,b));
+    }
+}
---
+++ Operation.java
@@ -0,0 +1,11 @@
+public class Operation {
+    public static int addition(int x, int y) {
+	return x + y;
+    }
+    public static int soustraction(int x, int y) {
+	return x - y;
+    }	
+    public static int produit(int x, int y) {
+	return x * y;
+    }
+}
