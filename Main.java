bb5d5d7b42ed17f99be0cb365dfef22e7eaa95bf
Slim Kanoun <slim.kanoun@enis.tn>
2025-10-14 18:05:17 +0100

    Programme de test avec affichage des valeurs des operants

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String... arg) {
 	int a = 2;
 	int b = 3;
-	System.out.println("a + b =" + Operation.addition(a,b));
-	System.out.println("a - b =" + Operation.soustraction(a,b));
-	System.out.println("a * b =" + Operation.produit(a,b));
+	System.out.println(a + " + " + b + " = " + Operation.addition(a,b));
+	System.out.println(a + " - " + b + " = " + Operation.soustraction(a,b));
+	System.out.println(a + " * " + b + " = " + Operation.produit(a,b));
     }
 }
